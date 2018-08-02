003f72f6ee3a1e98c75c11bb4858eba9478696cc
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-08-02 16:18:28 -0300

    commit 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-// Add your JavaScript
+$(document).ready(function() {
+
+   $.each(restaurantes, function (index, restaurante) {
+     $("<img>").attr("src", restaurante.image).appendTo(".img-restaurant");
+})  
+});
+
+var map;
+function initMap() {
+ map = new google.maps.Map(document.getElementById('map'), {
+ zoom: 15,
+ center: new google.maps.LatLng(-23.5578108, -46.6625469),
+ mapTypeId: 'roadmap'
+});
+
+var features = restaurantes.map(function(r){
+   return {
+     position: new google.maps.LatLng(r.latitude, r.longitude),
+     type: 'info'
+   }
+ });
+
+ // Create markers
+ features.forEach(function(feature) {
+   var marker = new google.maps.Marker({
+     position: feature.position,
+     map: map
+   });
+ });
+}
