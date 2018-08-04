411991e6db0bd89cfa8ec33e24c8e994c9a07dd0
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-08-04 18:39:39 -0300

    commit 2

--- js/app.js
+++ js/app.js
@@ -1,10 +1,55 @@
 $(document).ready(function() {
+  var searchRest = $('.search-rest');
+  var allRest = Object.keys(restaurantes);
 
-   $.each(restaurantes, function (index, restaurante) {
-     $("<img>").attr("src", restaurante.image).appendTo(".img-restaurant");
-})  
+  showAllRest();
+  searchRest.keyup(searchFilter);
+  $('.cover').mouseover(showCover).mouseout(hideCover);
+  $('.cover').click(fillModal);
+
+  function showAllRest() {
+    $.each(allRest, function(i) {
+      var restThumb = `<li class="all-images col-xs-6 col-sm-4 col-md-3"><a id="${allRest[i]}" href="#" data-toggle="modal" data-target="#infoModal">
+                        <span class="cover">
+                          <span>${restaurantes[allRest[i]].name}</span>
+                          </span>
+                        </a>
+                      </li>`;
+      $('.search-results .row ul').append(restThumb);
+      $('#' + allRest[i]).css({
+        'background-image': 'url(' + restaurantes[allRest[i]].image + ')'});
+    });
+  }
+
+  function showCover() {
+    $(this).css('opacity', '1');
+  }
+
+  function hideCover() {
+    $(this).css('opacity', '0');
+  }
+
+  function searchFilter() {
+    var searchLetter = searchRest.val();
+    $('.all-images').hide();
+    $('.all-images a').each(function() {
+      var typeArr = restaurantes[$(this).attr('id')].type;
+      for (var i = 0; i < typeArr.length; i++) {
+        if (typeArr[i].indexOf(searchLetter) !== -1 || ((restaurantes[$(this).attr('id')].type).toLowerCase()).indexOf(searchLetter.toLowerCase()) !== -1 || ((restaurantes[$(this).attr('id')].name).toLowerCase()).indexOf(searchLetter.toLowerCase()) !== -1) {
+          $(this).parent().fadeIn('fast');
+        }
+      }
+    });
+  }
+
+  // Modal
+  function fillModal() {
+    $('.modal-title').text(restaurantes[$(this).parent().attr('id')].name);
+    $('.descr').text(restaurantes[$(this).parent().attr('id')].description);
+  }
 });
 
+// Map
 var map;
 function initMap() {
  map = new google.maps.Map(document.getElementById('map'), {
@@ -15,8 +60,7 @@ function initMap() {
 
 var features = restaurantes.map(function(r){
    return {
-     position: new google.maps.LatLng(r.latitude, r.longitude),
-     type: 'info'
+     position: new google.maps.LatLng(r.latitude, r.longitude)
    }
  });
 
