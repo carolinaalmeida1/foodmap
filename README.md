d2aab0c1a21f80e5685c0cfeec621cb1797d5072
carolinaalmeida1 <carolina.almeida1@outlook.com>
2018-10-25 16:43:33 -0300

    v. 2

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Foodmap
 
-<!-- ## Motivo -->
-
 ### Desafio do Sprint 3 do bootcamp Laboratoria
 
 O desafio era desenvolver uma webapp que fosse possível localizar restaurantes específicos na região da Avenida Paulista, São Paulo - SP, utlizando a API da Google Maps. A lista de restaurantes foi passada como um banco de dados interno.
 
-![Screenshot](assets/images/screencapture-foodmap.png)
+![Screenshot](docs/mockups/screencapture-foodmap-1.png)
+
+![Screenshot](docs/mockups/screencapture-foodmap-2.png)
+
+![Screenshot](docs/mockups/screencapture-foodmap-3.png)
--- js/app.js
+++ js/app.js
@@ -1,43 +1,44 @@
-$(document).ready(function() {
-  $('.home').delay('3000').fadeIn('slow');
+$(document).ready(function () {
+  $(".home").delay("3000").fadeIn("slow");
 
-  var searchRest = $('.search-rest');
+  var searchRest = $(".search-rest");
   var allRest = Object.keys(restaurantes);
 
   showAllRest();
   searchRest.keyup(searchFilter);
-  $('.cover').mouseover(showCover).mouseout(hideCover);
-  $('.cover').click(fillModal);
+  $(".cover").mouseover(showCover).mouseout(hideCover);
+  $(".cover").click(fillModal);
 
   function showAllRest() {
-    $.each(allRest, function(i) {
+    $.each(allRest, function (i) {
       var restThumb = `<li class="all-images col-12 col-sm-12 col-md-3"><a id="${allRest[i]}" href="#" data-toggle="modal" data-target="#infoModal">
                         <span class="cover">
                           <span class="name">${restaurantes[allRest[i]].name}</span>
                           </span>
                         </a>
                       </li>`;
-      $('.search-results .row ul').append(restThumb);
-      $('#' + allRest[i]).css({
-        'background-image': 'url(' + restaurantes[allRest[i]].image + ')'});
+      $(".search-results .row ul").append(restThumb);
+      $("#" + allRest[i]).css({
+        "background-image": "url(" + restaurantes[allRest[i]].image + ")"
+      });
     });
   }
 
   function showCover() {
-    $(this).css('opacity', '1');
+    $(this).css("opacity", "1");
   }
   function hideCover() {
-    $(this).css('opacity', '0');
+    $(this).css("opacity", "0");
   }
 
   function searchFilter() {
     var searchLetter = searchRest.val();
-    $('.all-images').hide();
-    $('.all-images a').each(function() {
-      var typeArr = restaurantes[$(this).attr('id')].type;
+    $(".all-images").hide();
+    $(".all-images a").each(function () {
+      var typeArr = restaurantes[$(this).attr("id")].type;
       for (var i = 0; i < typeArr.length; i++) {
-        if (typeArr[i].indexOf(searchLetter) !== -1 || ((restaurantes[$(this).attr('id')].type).toLowerCase()).indexOf(searchLetter.toLowerCase()) !== -1 || ((restaurantes[$(this).attr('id')].name).toLowerCase()).indexOf(searchLetter.toLowerCase()) !== -1) {
-          $(this).parent().fadeIn('fast');
+        if (typeArr[i].indexOf(searchLetter) !== -1 || ((restaurantes[$(this).attr("id")].type).toLowerCase()).indexOf(searchLetter.toLowerCase()) !== -1 || ((restaurantes[$(this).attr("id")].name).toLowerCase()).indexOf(searchLetter.toLowerCase()) !== -1) {
+          $(this).parent().fadeIn("fast");
         }
       }
     });
@@ -45,10 +46,10 @@ $(document).ready(function() {
 
   // Modal
   function fillModal() {
-    $('.modal-title').text(restaurantes[$(this).parent().attr('id')].name);
-    $('.type-title').text(restaurantes[$(this).parent().attr('id')].type);
-    $('.descr').text(restaurantes[$(this).parent().attr('id')].description);
-    $('.img-modal').text(restaurantes[$(this).parent().attr('id')].image);
+    $(".modal-title").text(restaurantes[$(this).parent().attr("id")].name);
+    $(".type-title").text(restaurantes[$(this).parent().attr("id")].type);
+    $(".descr").text(restaurantes[$(this).parent().attr("id")].description);
+    $(".img-modal").text(restaurantes[$(this).parent().attr("id")].image);
   }
 
 });
@@ -56,23 +57,23 @@ $(document).ready(function() {
 // Map
 var map;
 function initMap() {
- map = new google.maps.Map(document.getElementById('map'), {
- zoom: 15,
- center: new google.maps.LatLng(-23.5578108, -46.6625469),
- mapTypeId: 'roadmap'
-});
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: new google.maps.LatLng(-23.5578108, -46.6625469),
+    mapTypeId: "roadmap"
+  });
 
-var features = restaurantes.map(function(r){
-   return {
-     position: new google.maps.LatLng(r.latitude, r.longitude)
-   }
- });
+  var features = restaurantes.map(function (r) {
+    return {
+      position: new google.maps.LatLng(r.latitude, r.longitude)
+    }
+  });
 
- // Create markers
- features.forEach(function(feature) {
-   var marker = new google.maps.Marker({
-     position: feature.position,
-     map: map
-   });
- });
+  // Create markers
+  features.forEach(function (feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      map: map
+    });
+  });
 }
